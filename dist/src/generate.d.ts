bfb8998e99663bb7ff472c686aec251ec34aa453
Paul Schleifer <paul_schleifer@hotmail.com>
2026-02-11 18:49:18 +0000

    Clean repo: remove build artefacts and node_modules

--- dist/src/generate.d.ts
+++
@@ -1,3 +0,0 @@
-import type { IndividualAttributes } from "./types";
-import type { Archetype } from "./archetypes";
-export declare function generateIndividual(seedU32: number, arch: Archetype): IndividualAttributes;
--- dist/src/lod.d.ts
+++
@@ -1,9 +0,0 @@
-import type { IndividualAttributes } from "./types";
-import { type Q } from "./units";
-export interface SquadAggregate {
-    count: number;
-    mean: IndividualAttributes;
-    cohesion: Q;
-    training: Q;
-}
-export declare function aggregateSquad(members: IndividualAttributes[]): SquadAggregate;
